98c334150e3c03f075f597223c698c5f0e64d4aa
debanjan2004 <127493318+debanjan2004@users.noreply.github.com>
2024-01-17 23:52:24 +0530

    Add files via upload

---
+++ combination_of_123.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class combination_of_123
+{
+	public static void main(String[] args) {
+	   for(int i=1;i<=3;i++){
+	       for(int j=1;j<=3;j++){
+	           for(int k=1;k<=3;k++){
+	   if(i!=j&&i!=k&&j!=k){
+	       System.out.println(i+""+j+""+k);
+	   }
+	           }
+	       }
+	   }
+	}
+}
