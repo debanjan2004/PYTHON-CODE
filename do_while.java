47fcc91628632458d793718cf72ec388f81e4e22
debanjan2004 <127493318+debanjan2004@users.noreply.github.com>
2024-01-17 23:53:01 +0530

    Add files via upload

---
+++ do_while.java
@@ -0,0 +1,10 @@
+public class do_while {
+    public static void main(String[]args){
+        int a=0;
+        int b=10;
+        do{
+            System.out.println(a);
+            a++;        }
+        while(a<=b);
+    }
+}
